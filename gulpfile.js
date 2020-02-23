11db452c547a8f0d0f0223206f61288c45ed8cb3
Alekseyesss <alewa86@gmail.com>
2020-02-23 21:40:29 +0200

    added 404, about, contact

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,7 @@ gulp.task('style', function () {
         'node_modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/rateyo/src/jquery.rateyo.css',
         'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -37,7 +38,8 @@ gulp.task('script', function () {
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
-        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',  
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',  
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
@@ -68,4 +70,6 @@ gulp.task('watch', function () {
     gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
 
-gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync'))
+gulp.task('default', gulp.parallel('watch', 'browser-sync'))
+
+// gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync'))
